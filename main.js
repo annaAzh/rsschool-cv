daf6d3e306fad5ee05e513866910a74c48980c08
annaAzh <forarre@yandex.ru>
2023-07-06 09:22:14 +0300

    fix: fix in js file (change window on document for listener)

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
   /*----------------open and close hamburger menu--------------*/
